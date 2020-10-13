2dd324d7797d8b2f1ac20b41ab2c6a05a31ee1a6
Diego Di Leonforte <dileonfortediego@gmail.com>
2020-10-13 18:56:48 -0300

    Agregue slides de imagenes responsive

---
+++ js/main.js
@@ -0,0 +1,185 @@
+$('.slidersw').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.slidermb').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.sliderpk').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.slidercel').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.slidertab').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  $('.sliderpc').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          autoplay: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 678,
+        settings: {
+          autoplay: true,
+          dots: true,
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
